db1630a484b027efb53a680b24ca50d1898093f9
mobeenatif123456 <73457508+mobeenatif123456@users.noreply.github.com>
2022-05-09 11:45:19 +0500

    some profile changes

--- config/profile.js
+++ config/profile.js
@@ -1,5 +1,4 @@
 const profileTestEnvironment = require('./profile-test-environment');
-const profilePakCloud = require('./profile-pakcloud-environment');
 const profileAWSProduction = require('./profile-production-environment');
 
 // Mention profile to be used during execution of test cases.
