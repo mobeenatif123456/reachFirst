456178168d8ccde7c7c4534a2fb4c8c1915b7295
mobeenatif123456 <73457508+mobeenatif123456@users.noreply.github.com>
2022-05-10 09:39:25 +0500

    Cleaning up code base

--- config/profile-test-environment.js
+++ config/profile-test-environment.js
@@ -10,63 +10,6 @@ module.exports = {
 
   network1Admin1: {email: 'admin@testsuite1.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
 
-  network2User1: {email: 'user2@testsuite2.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network2User2: {email: 'user3@testsuite2.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network2Admin1: {email: 'admin@testsuite2.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-  
-  network3User1: {email: 'user2@testsuite3.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network3User2: {email: 'user3@testsuite3.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network3Admin1: {email: 'admin@testsuite3.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network4User1: {email: 'user2@testsuite4.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network4User2: {email: 'user3@testsuite4.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network4Admin1: {email: 'admin@testsuite4.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network5User1: {email: 'user2@testsuite5.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network5User2: {email: 'user3@testsuite5.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network5Admin1: {email: 'admin@testsuite5.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network6User1: {email: 'user2@testsuite6.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network6User2: {email: 'user3@testsuite6.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network6Admin1: {email: 'admin@testsuite6.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network7User1: {email: 'user2@testsuite7.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network7User2: {email: 'user3@testsuite7.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network7Admin1: {email: 'admin@testsuite7.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network8User1: {email: 'user2@testsuite8.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network8User2: {email: 'user3@testsuite8.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-  network8User3: {email: 'user1@testsuite8.com', password: 'Kitchen1', firstName: 'User', lastName: 'One'},
-
-  network8Admin1: {email: 'admin@testsuite8.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network9User1: {email: 'user2@testsuite9.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network9User2: {email: 'user3@testsuite9.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network9Admin1: {email: 'admin@testsuite9.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  network10User1: {email: 'user2@testsuite10.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  network10User2: {email: 'user3@testsuite10.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  network10Admin1: {email: 'admin@testsuite10.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  unifiedNetwork1User1: {email: 'user2@unified1.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  unifiedNetwork1User2: {email: 'user3@unified1.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  unifiedNetwork1Admin1: {email: 'admin@unified1.com', password: 'kitchen1', firstName: 'Admin', lastName: 'First'},
-
-  unifiedNetwork2User1: {email: 'user2@testsuite10.com', password: 'Kitchen1', firstName: 'User', lastName: 'Two'},
-  unifiedNetwork2User2: {email: 'user3@testsuite10.com', password: 'Kitchen1', firstName: 'User', lastName: 'Three'},
-
-  unifiedNetwork2Admin1: {email: 'admin@unified2.com', password: 'kitchen1', firstName: 'Admin', lastName: 'First'}
-
-
 
   
 }
