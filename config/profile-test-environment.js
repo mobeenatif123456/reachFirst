34df244b0b0b9a4668b1a54a526bd967c7adca14
mobeenatif123456 <73457508+mobeenatif123456@users.noreply.github.com>
2022-05-12 11:46:54 +0500

    login

--- config/profile-test-environment.js
+++ config/profile-test-environment.js
@@ -3,6 +3,7 @@ module.exports = {
   url: 'https://qa-web.worktually.com/',
 
   signupDomain: 'guerrillamail.info', // => /test/specs/signup
+  
   employer1: {email: 'admin@testsuite11.com', password: 'Kitchen1', firstName: 'Admin', lastName: 'First'},
 
 }
